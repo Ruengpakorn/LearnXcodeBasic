d44665e29383d2e7ab5b14dd67c248ab705a823f
Nick <ruengpakorn@nicks-macbook-pro.local>
2018-11-21 09:46:08 +0700

    add new print say my name

---
+++ LearnXcodeBasic/WE525/WE525/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WE525
+//
+//  Created by nick ruengpakorn on 21/11/2561 BE.
+//  Copyright © 2561 nick ruengpakorn. All rights reserved.
+//
+
+import UIKit
+    /// nick add first commit
+    func sayMyName(){
+    print("Hi mango")
+        print("Hi nick")
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ xc14_PushViewRefactor_Storyboard/xc14_PushViewRefactor_Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xc14_PushViewRefactor_Storyboard
+//
+//  Created by nick ruengpakorn on 19/11/2561 BE.
+//  Copyright © 2561 nick ruengpakorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
